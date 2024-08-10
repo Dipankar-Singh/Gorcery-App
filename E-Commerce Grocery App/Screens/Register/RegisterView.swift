9eb5f1a417419775134a455ae5c517dcb1d494c5
Dipankar Singh <dipankarofficial2123@gmail.com>
2024-08-11 00:14:12 +0530

    Created the First View of the Application

---
+++ E-Commerce Grocery App/Screens/Register/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 10/08/24.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
