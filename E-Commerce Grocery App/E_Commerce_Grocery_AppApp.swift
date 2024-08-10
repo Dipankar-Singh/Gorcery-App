6d76044639a2ece0c77d9d273c5d74da66c25c0c
Dipankar Singh <dipankarofficial2123@gmail.com>
2024-08-10 20:17:22 +0530

    Initial Commit

---
+++ E-Commerce Grocery App/E_Commerce_Grocery_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_Grocery_AppApp.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_Grocery_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
