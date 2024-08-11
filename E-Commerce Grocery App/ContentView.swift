f9371345b516a7a4ccd07b72f28f4b0bcb4b4b04
Dipankar Singh <dipankarofficial2123@gmail.com>
2024-08-11 18:45:57 +0530

    Fix A Design issue for small screens

--- E-Commerce Grocery App/ContentView.swift
+++ E-Commerce Grocery App/ContentView.swift
@@ -9,10 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Learning A New Begning")
+        NavigationStack{
+            VStack {
+                Image("shopingCart").resizable().scaledToFit()
+                Spacer()
+                Text("Your grocery needs made simple with a user friendly app for quick and easy shopping.").font(.system(size: 25,weight: .semibold,design: .rounded)).multilineTextAlignment(.center).foregroundColor(.gray)
+                Spacer()
+                HStack(spacing: 20){
+                    NavigationLink(destination: RegisterView()){
+                        Text("SignUp").frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .padding(.vertical,18)
+                            .background(Color.purple)
+                            .foregroundColor(.white)
+                            .font(.system(size: 25,weight: .semibold,design: .rounded))
+                            .cornerRadius(.infinity)
+                    }
+                    NavigationLink(destination: LoginView()){
+                        Text("Login").frame(maxWidth: .infinity)
+                            .padding(.vertical,18)
+                            .background(Color.purple)
+                            .foregroundColor(.white)
+                            .font(.system(size: 25,weight: .semibold,design: .rounded))
+                            .cornerRadius(.infinity)
+                    }
+                }
+            }.padding()
         }
-        .padding()
     }
 }
 
--- E-Commerce Grocery App/Screens/Register/RegisterView.swift
+++ E-Commerce Grocery App/Screens/Register/RegisterView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name:String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Spacer()
+                Text("Create Your \n Account")
+                    .font(.system(size: 40, weight: .semibold, design: .rounded))
+                    .padding(.leading, 5)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                Spacer()
+                VStack(spacing: 20, content: {
+                    Spacer()
+                    TextField("", text: $name, prompt: Text("Enter Email").foregroundColor(.white))
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(RoundedRectangle(cornerRadius: 30).stroke(Color.white, lineWidth: 2))
+                        .font(.system(size: 25, weight: .medium, design: .rounded))
+                    TextField("", text: $name, prompt: Text("Enter Email").foregroundColor(.white))
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(RoundedRectangle(cornerRadius: 30).stroke(Color.white, lineWidth: 2))
+                        .font(.system(size: 25, weight: .medium, design: .rounded))
+                    TextField("", text: $name, prompt: Text("Enter Email").foregroundColor(.white))
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(RoundedRectangle(cornerRadius: 30).stroke(Color.white, lineWidth: 2))
+                        .font(.system(size: 25, weight: .medium, design: .rounded))
+                    TextField("", text: $name, prompt: Text("Enter Email").foregroundColor(.white))
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(RoundedRectangle(cornerRadius: 30).stroke(Color.white, lineWidth: 2))
+                        .font(.system(size: 25, weight: .medium, design: .rounded))
+                    NavigationLink(destination: LoginView()) {
+                        Text("Submit")
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 18)
+                            .background(Color.white)
+                            .foregroundColor(.purple)
+                            .font(.system(size: 25, weight: .semibold, design: .rounded))
+                            .cornerRadius(.infinity)
+                            .padding(.horizontal, 40)
+                            .padding()
+                    }
+                    Spacer()
+                }).padding().ignoresSafeArea()
+                    .background(.purple).clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                
+            }
+            .ignoresSafeArea(edges:.bottom)
+            
+        }
     }
 }
 
 #Preview {
     RegisterView()
 }
+
