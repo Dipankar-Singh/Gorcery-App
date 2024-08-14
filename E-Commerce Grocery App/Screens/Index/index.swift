5ff7fd004c799cebcbdb148c8052adb67e987aca
Dipankar Singh <dipankarofficial2123@gmail.com>
2024-08-15 00:17:09 +0530

    Created the index Page and added the layout

---
+++ E-Commerce Grocery App/Screens/Index/index.swift
@@ -0,0 +1,18 @@
+//
+//  index.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 12/08/24.
+//
+
+import SwiftUI
+
+struct index: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    index()
+}
---
+++ E-Commerce Grocery App/Screens/SharedViews/SellingCard.swift
@@ -0,0 +1,18 @@
+//
+//  SellingCard.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 14/08/24.
+//
+
+import SwiftUI
+
+struct SellingCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SellingCard()
+}
