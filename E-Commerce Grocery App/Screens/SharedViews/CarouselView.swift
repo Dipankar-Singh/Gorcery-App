0543186f276a7e085cbb2cdd672b0d6ab81922a9
Dipankar Singh <dipankarofficial2123@gmail.com>
2024-08-16 22:43:19 +0530

    Added A Carosel

---
+++ E-Commerce Grocery App/Screens/SharedViews/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 15/08/24.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
