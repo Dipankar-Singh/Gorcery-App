2b45556c995243145d9e73c6bfdb071412b7dd52
Dipankar Singh <dipankarofficial2123@gmail.com>
2024-08-21 23:50:23 +0530

    Added the Item Description On Click of card Also

---
+++ E-Commerce Grocery App/Screens/ItemDescription/itemDescViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  itemDescViewModel.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 21/08/24.
+//
+
+import Foundation
---
+++ E-Commerce Grocery App/Screens/ItemDescription/itemDescriptionModel.swift
@@ -0,0 +1,8 @@
+//
+//  itemDescriptionModel.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 21/08/24.
+//
+
+import Foundation
