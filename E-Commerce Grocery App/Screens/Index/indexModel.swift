0a0705d2fce1a5cfcc631b8c6a00ba2322e77baa
Dipankar Singh <dipankarofficial2123@gmail.com>
2024-08-21 09:56:57 +0530

    Updated

---
+++ E-Commerce Grocery App/Screens/Index/indexModel.swift
@@ -0,0 +1,8 @@
+//
+//  indexModel.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 20/08/24.
+//
+
+import Foundation
---
+++ E-Commerce Grocery App/Screens/ItemDescription/indexItemDescription.swift
@@ -0,0 +1,18 @@
+//
+//  indexItemDescription.swift
+//  E-Commerce Grocery App
+//
+//  Created by Dipankar Singh on 17/08/24.
+//
+
+import SwiftUI
+
+struct indexItemDescription: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    indexItemDescription()
+}
